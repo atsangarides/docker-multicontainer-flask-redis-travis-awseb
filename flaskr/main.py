07d0fc6c2151848155abfe3b42a485bdac206567
Andreas <andreas@multiply.ai>
2020-05-07 19:36:07 +0100

    fixed prod configuration

--- flaskr/main.py
+++ flaskr/main.py
@@ -2,7 +2,7 @@ import os
 
 from app import create_app
 
-app = create_app('config.DevelopmentConfig')
+app = create_app(os.environ.get('CONFIG'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=os.environ.get('DEBUG'), threaded=True)
